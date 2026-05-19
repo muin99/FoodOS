01f33cff2818899f97eafcd7416a7a008237117c
mashfiqur05 <mashfiqur07@gmail.com>
2026-05-20 02:45:41 +0600

    fixed marked as solved feature

--- admin/controller/complaintController.php
+++ admin/controller/complaintController.php
@@ -3,7 +3,6 @@
 include '../../dirCommon/dbconnect.php';
 include '../model/complaintModel.php';
 
-/* Resolve complaint */
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resolve_id'])) {
 
     resolveComplaint($conn, $_POST['resolve_id']);
@@ -11,11 +10,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resolve_id'])) {
     header("Location: complaints.php");
     exit;
 }
-
-/* Load complaints */
 $complaints = getAllComplaints($conn);
 
-/* Selected complaint */
 $selectedComplaint = null;
 
 if (isset($_GET['view_id'])) {
